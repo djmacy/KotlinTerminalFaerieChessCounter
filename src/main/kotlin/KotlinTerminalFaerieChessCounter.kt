761c77ec0d00004c45051cb7eeeaa6e00392dd9b
David Macy <dmacy@carroll.edu>
2023-10-29 21:02:57 -0600

    add multiple values to enum piece class

--- src/main/kotlin/KotlinTerminalFaerieChessCounter.kt
+++ src/main/kotlin/KotlinTerminalFaerieChessCounter.kt
@@ -7,26 +7,26 @@ fun main() {
     FaerieChessCounter().faerieChessCounter()
 }
 
-enum class PieceValue(val value: Int) {
-    PAWN(1),
-    PEASANT(2),
-    SOLDIER(3),
-    ROOK(9),
-    KNIGHT(4),
-    BISHOP(6),
-    CATAPULT(3),
-    CHAMBERLAIN(6),
-    COURTESAN(6),
-    HERALD(6),
-    INQUISITOR(8),
-    LANCER(5),
-    PONTIFF(8),
-    THIEF(5),
-    TOWER(10),
-    QUEEN(12),
-    JESTER(12),
-    KING(0),
-    REGENT(15)
+enum class Pieces(val value: Int, val minPieces: Int, val maxPieces: Int) {
+    PAWN(1, 4, 8),
+    PEASANT(2, 0, 2),
+    SOLDIER(3, 0, 2),
+    ROOK(9, 0, 2),
+    KNIGHT(4, 0, 2),
+    BISHOP(6, 0, 2),
+    CATAPULT(3, 0, 1),
+    CHAMBERLAIN(6, 0, 1),
+    COURTESAN(6, 0, 1),
+    HERALD(6, 0, 1),
+    INQUISITOR(8, 0, 1),
+    LANCER(5, 0, 1),
+    PONTIFF(8, 0, 1),
+    THIEF(5, 0, 1),
+    TOWER(10, 0, 1),
+    QUEEN(12, 0, 1),
+    JESTER(12, 0, 1),
+    KING(0, 0, 1),
+    REGENT(15, 0, 1)
 }
 
 class FaerieChessCounter {
@@ -87,17 +87,14 @@ class FaerieChessCounter {
     private fun calculateRankIPoints(): Int {
         var rank1 = 0
         var totalPoints = 0
-        //This is the maximum number of pieces available for each piece.
-        val pawnMin = 4
-        val pawnLimit = 8
-        val soldierLimit = 2
+        val soldierLimit = Pieces.SOLDIER.maxPieces
         //The user can only select 8 rank I pieces so it will break when the user selects more than 8
         while (true) {
-            val pawn = checkForValidClassicalInput("How many pawns would you like: ", pawnMin, pawnLimit)
+            val pawn = checkForValidClassicalInput("How many pawns would you like: ", Pieces.PAWN.minPieces, Pieces.PAWN.maxPieces)
             var peasant = 0
             var soldier = 0
             //This variable will keep track of how many points the user has until the end of the script
-            totalPoints += pawn * PieceValue.PAWN.value
+            totalPoints += pawn * Pieces.PAWN.value
             //rank_1 will determine when we have selected the right number of pieces. 8 is the maximum number of rank_1 pieces
             rank1 += pawn
 
@@ -107,7 +104,7 @@ class FaerieChessCounter {
             } else if (rank1 == 4) {
                 peasant = 2
                 soldier = 2
-                totalPoints += (peasant * PieceValue.PEASANT.value + soldier * PieceValue.SOLDIER.value)
+                totalPoints += (peasant * Pieces.PEASANT.value + soldier * Pieces.SOLDIER.value)
                 println("Due to picking 4 pawns you will automatically get 2 peasants and 2 soldiers")
                 break
             } else if (rank1 > 4) {
@@ -115,7 +112,7 @@ class FaerieChessCounter {
                 val peasantLimit = minOf(2, 8 - rank1)
                 println("You can select from $peasantMin to $peasantLimit peasants")
                 peasant = checkForValidClassicalInput("How many peasants would you like: ", peasantMin, peasantLimit)
-                totalPoints += peasant * PieceValue.PEASANT.value
+                totalPoints += peasant * Pieces.PEASANT.value
                 rank1 += peasant
                 if (rank1 == 8) {
                     println("You have picked the maximum number of pieces")
@@ -123,12 +120,12 @@ class FaerieChessCounter {
                 } else if (rank1 == 6) {
                     println("You automatically get 2 soldiers")
                     soldier = 2
-                    totalPoints += soldier * PieceValue.SOLDIER.value
+                    totalPoints += soldier * Pieces.SOLDIER.value
                     rank1 += soldier
                 } else if (rank1 == 7) {
                     println("You automatically get 1 soldier")
                     soldier = 1
-                    totalPoints += soldier * PieceValue.SOLDIER.value
+                    totalPoints += soldier * Pieces.SOLDIER.value
                     rank1 += soldier
                 }
             }
@@ -142,58 +139,38 @@ class FaerieChessCounter {
      * @param totalPoints - Total Points from selecting the rank I pieces
      */
     private fun calculateRankII(totalPoints: Int): Int {
-        //classical_limit is 2 because in the original game of chess you have 2 rooks, 2 bishops, and 2 knights
-        val classicalMin = 0
-        val classicalLimit = 2
-        val nonClassicalPieces = listOf<String>("catapult", "chamberlain", "courtesan", "herald",
-                                                "inquisitor", "lancer", "pontiff", "thief", "tower")
-        val pieceValueMap = mapOf(
-            "catapult" to PieceValue.CATAPULT,
-            "chamberlain" to PieceValue.CHAMBERLAIN,
-            "courtesan" to PieceValue.COURTESAN,
-            "herald" to PieceValue.HERALD,
-            "inquisitor" to PieceValue.INQUISITOR,
-            "lancer" to PieceValue.LANCER,
-            "pontiff" to PieceValue.PONTIFF,
-            "thief" to PieceValue.THIEF,
-            "tower" to PieceValue.TOWER
-        )
 
+        val rankIIPieces = listOf<Pieces>(
+            Pieces.ROOK, Pieces.KNIGHT, Pieces.BISHOP, Pieces.CATAPULT,
+            Pieces.CHAMBERLAIN, Pieces.COURTESAN, Pieces.HERALD,
+            Pieces.INQUISITOR, Pieces.LANCER, Pieces.PONTIFF, Pieces.THIEF,
+            Pieces.TOWER
+        );
         while (true) {
-            //This will make sure that we get to six total pieces selected if loop is restarted
             var rank2 = 0
-            //restart the total points calculated to what the total was after rank_1 calculation if loop gets reset to
             var rank2StartingPoints = totalPoints
 
-            val rook = checkForValidClassicalInput("How many rooks would you like: ", classicalMin, classicalLimit)
-            rank2StartingPoints += rook * PieceValue.ROOK.value
-            rank2 += rook
-
-            val knight = checkForValidClassicalInput("How many knights would you like: ", classicalMin, classicalLimit)
-            rank2StartingPoints += knight * PieceValue.KNIGHT.value
-            rank2 += knight
-
-            val bishop = checkForValidClassicalInput("How many bishops would you like: ", classicalMin, classicalLimit)
-            rank2StartingPoints += bishop * PieceValue.BISHOP.value
-            rank2 += bishop
-            if (rank2 >= 6) {
-                return rank2StartingPoints
-            }
-
-            //There is only one of each piece that you can select from after you are done selecting from rooks, knights, and bishops.
-            println("\nFor the rest of these insert 'y' for yes and 'n' for no")
-            for (piece in nonClassicalPieces) {
-                val selectedPiece = checkForValidFaerieInput("Would you like a $piece?")
-                val pieceValue = pieceValueMap[piece]
-                if (pieceValue != null) {
-                    rank2StartingPoints += selectedPiece * (pieceValue.value)
+            for (piece in rankIIPieces) {
+                if (piece.maxPieces == 2) {
+                    val selectedPieces = checkForValidClassicalInput(
+                        "How many $piece" + "s would you like: ", piece.minPieces, piece.maxPieces
+                    )
+                    rank2StartingPoints += selectedPieces * piece.value
+                    rank2 += selectedPieces
+                    if (rank2 == 6) {
+                        break
+                    }
+                } else if (piece.maxPieces == 1) {
+                    println("\nFor the rest of these insert 'y' for yes and 'n' for no")
+                    val selectedPiece = checkForValidFaerieInput("Would you like a ${piece.name}?")
+                    rank2StartingPoints += selectedPiece * (piece.value)
                     rank2 += selectedPiece
                     if (rank2 == 6) {
                         break
                     }
                 }
             }
-            if (rank2 >= 6) {
+            if (rank2 == 6) {
                 return rank2StartingPoints
             } else {
                 println("You only selected $rank2 pieces. Be sure to select 6 rank II pieces. The count will now reset!")
@@ -201,7 +178,7 @@ class FaerieChessCounter {
             }
         }
     }
-
+    
     /**
      * This method will calculate how many points the user has after selecting all of their rank III pieces.
      */
@@ -214,20 +191,20 @@ class FaerieChessCounter {
             val queen = readLine()?.lowercase()
             if (queen == "y") {
                 val queen = 1
-                currentPoints += queen * PieceValue.QUEEN.value
+                currentPoints += queen * Pieces.QUEEN.value
             } else if (queen == "n") {
                 println("Since you did not select a queen, you automatically get a jester")
                 val jester = 1
-                currentPoints += jester * PieceValue.JESTER.value
+                currentPoints += jester * Pieces.JESTER.value
             } else {
                 println("Invalid input. Please select 'y' or 'n'")
                 continue
             }
             //It will only allow the user to pick if he has enough points for a regent and even then the user can select whether he/she wants it or not
-            if (maxPoints - currentPoints <= PieceValue.REGENT.value) {
+            if (maxPoints - currentPoints <= Pieces.REGENT.value) {
                 println("You do not have enough points for a regent, so you will have to get a king.")
                 val king = 1
-                currentPoints += king * PieceValue.KING.value
+                currentPoints += king * Pieces.KING.value
                 rank3 += king
                 break
             }
@@ -235,11 +212,11 @@ class FaerieChessCounter {
             val king = readLine()?.lowercase()
             if (king == "y") {
                 val king = 1
-                currentPoints += king * PieceValue.KING.value
+                currentPoints += king * Pieces.KING.value
             } else if (king == "n") {
                 println("Since you did not select a king, you automatically get a regent")
                 val regent = 1
-                currentPoints += regent * PieceValue.KING.value
+                currentPoints += regent * Pieces.REGENT.value
                 break
             } else {
                 println("Invalid input. Please select 'y' or 'n'")
